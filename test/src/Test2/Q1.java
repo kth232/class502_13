fc941b0e98332d7c3b32d75aa0b9ab33aa395af2
kth232 <kth7537@naver.com>
2024-04-30 17:29:45 +0900

    중간평가

--- test/src/Test2/Q1.java
+++ test/src/Test2/Q1.java
@@ -5,6 +5,12 @@ public class Q1 {
         int num1=20;
         double num2=3.0;
         int result = (int)(num1*num2);
-        System.out.println(result);
+        System.out.printf("곱셈: %d%n", result);
+        int result2 = (int)(num1+num2);
+        System.out.printf("덧셈: %d%n", result2);
+        int result3 = (int)(num1-num2);
+        System.out.printf("뺄셈: %d%n", result3);
+        int result4 = (int)(num1/num2);
+        System.out.printf("나눗셈: %d%n", result4);
     }
 }
--- test/src/Test3/Card/CardCompanyTest.java
+++ test/src/Test3/Card/CardCompanyTest.java
@@ -3,7 +3,7 @@ package Test3.Card;
 public class CardCompanyTest {
     public static void main(String[] args) {
 
-        CardCompany company = CardCompany.getInstance();  //싱글톤 패턴
+        CardCompany company = CardCompany.getInstance();  //싱글톤 패턴: 필요할 때 한개만 생성해서 자원 공유
 
         Card myCard = company.createCard();   //메서드에서 Card 생성
         Card yourCard = company.createCard();
