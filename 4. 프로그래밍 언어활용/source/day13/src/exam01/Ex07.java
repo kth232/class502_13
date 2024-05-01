6a7667c25c26dbdd0dac0b04b687093995830b40
kth232 <kth7537@naver.com>
2024-05-01 10:41:41 +0900

    컬렉션 프레임워크_Arrays

--- 4. 프로그래밍 언어활용/source/day13/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day13/src/exam01/Ex07.java
@@ -9,7 +9,7 @@ public class Ex07 {
         Integer[] nums2 = {22, 1, 5, 10, 96, 54};
         //기본 정렬, Integer 기본 정렬 기준-java.lang.Comparable, int comparTo(..)
         Arrays.sort(nums);
-        Arrays.sort(nums2, Comparator.reverseOrder()); //Comparator는 지네릭 타입->Comparator를 사용하려면 wrapper 클래스 형태로 묶어서 사용
+        Arrays.sort(nums2, Comparator.reverseOrder()); //Comparator는 지네릭 타입 사용->Comparator를 사용하려면 wrapper 클래스 형태로 묶어서 사용
         System.out.println(Arrays.toString(nums));
     }
 }
