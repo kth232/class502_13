6838860a10dd7bec1b89788eea473ee5fc708e25
kth232 <kth7537@naver.com>
2024-05-02 17:07:41 +0900

    스트림

--- 4. 프로그래밍 언어활용/source/day14/src/exam02/Ex08.java
+++ 4. 프로그래밍 언어활용/source/day14/src/exam02/Ex08.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class Ex08 {
     public static void main(String[] args) {
-        long cnt = Stream.of("name1", "name2", "name3").count(); //스트림 객체 생성
+        long cnt = Stream.of("name1", "name2", "name3").count(); // 일반 스트림 객체 생성
         System.out.println(cnt);
     }
 }
