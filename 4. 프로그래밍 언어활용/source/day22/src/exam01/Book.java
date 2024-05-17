e3e2d61ab36e02ae63641c59b165f4c7ca756250
kth232 <kth7537@naver.com>
2024-05-17 16:55:40 +0900

    File and Serialization

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Book.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Book.java
@@ -3,9 +3,18 @@ package exam01;
 import java.io.Serializable;
 
 public class Book implements Serializable { //문자 직렬화
+
+    private static final long serialVarsionUID =1L; //버전 관리
+
     private int isbn; //도서번호
     private String title; //도서명
     private transient String author; //저자, transient: 직렬화 배제(민감한 정보일 시)->null로 출력
+
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
 
     public Book(int isbn, String title, String author) {
         this.isbn = isbn;
