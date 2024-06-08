3390b027c72e0030519de5093d443239e4276d60
kth232 <kth7537@naver.com>
2024-06-08 14:47:58 +0900

    객체1 생성자 함수

--- 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex01.java
@@ -4,12 +4,12 @@ public class Ex01 {
     public static void main(String[] args) {
         Student s1 = new Student(); //s1: 지역변수, 참조변수 ->스택 메모리에 저장
         s1.id = 202411; //공간 할당 받아 변수(객체)가 됨
-        s1.name = "kim"; //id, name, subject는 힙 메모리에 저장
+        s1.name = "kim"; //id, name, subject: 인스턴스 변수, 멤버 변수 ->힙 메모리에 저장
         s1.subject = "JAVA";
         System.out.println(s1.id);
         s1.study(); //인스턴스 메서드
 
-        Student s2 = new Student();
+        Student s2 = new Student(); //Student: 참조 자료형
         s2.id = 202412;
         s2.name = "lee";
         s2.subject = "Java Script";
@@ -18,7 +18,7 @@ public class Ex01 {
         System.out.println("s1 == s2: "+ (s1 == s2)); // 다른 객체, 참조 주소 다름
 
         Student s3 = s2; //s3에 s2가 가지고 있는 주소 복사
-        s3.name="kang";
+        s3.name="park";
         s3.study();
 
         s2.study(); //s3과 같은 주소로 참조
@@ -29,7 +29,7 @@ public class Ex01 {
         s1.study();
         */
         
-        Student s4 = new Student(); // 생성자 함수의 초기값
+        Student s4 = new Student(); // 내부에서 객체 생성, 생성자 함수의 멤버 변수 초기화
         s4.study();
     }
 }
--- 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex03.java
+++ 4. 프로그래밍 언어활용/source/day02/src/exam01/Ex03.java
@@ -4,7 +4,7 @@ public class Ex03 {
     public static void main(String[] args) {
         Student s1 = new Student(202411, "lee", "java"); //매개변수만 입력, 인텔리제이에서 변수명에 대한 힌트 제공,,
         s1.study(); // 객체가 먼저 생성되어야 함수가 실행될 수 있다
-        System.out.println(System.identityHashCode(s1)); //변수의 가상주소 출력
+        System.out.println(System.identityHashCode(s1)); //변수의 가상주소 출력 -> JVM이 실행될 때 물리적 주소로 접근함
 
         Student s2 = new Student(202412, "kim", "react"); //새로운 다른 객체를 생성, 입력받는 값에 따라 다르게 출력
         s2.study();
