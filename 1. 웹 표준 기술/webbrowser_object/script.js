a2924840407b9b80d97317d440db34925421957c
kth232 <kth7537@naver.com>
2024-07-08 18:59:24 +0900

    webbrowser_object

--- 1. 웹 표준 기술/webbrowser_object/script.js
+++ 1. 웹 표준 기술/webbrowser_object/script.js
@@ -1,2 +1,4 @@
-const button = document.getElementById("button");
-console.log(button);
+//window.addEventListener("DOMContentLoaded", function() { //이벤트 바인딩
+    const button = document.getElementById("button");
+    console.log(button);
+//});
