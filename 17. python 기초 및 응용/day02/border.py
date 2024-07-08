351c4a37129482afce468a06596d5875dcc78559
kth232 <kth7537@naver.com>
2024-07-08 17:09:42 +0900

    파이썬 기초-숫자, 문자열

---
+++ 17. python 기초 및 응용/day02/border.py
@@ -0,0 +1,15 @@
+print("=" * 50)
+print("class 502\nfighting!")
+print("=" * 50)
+# 한 줄 주석
+'''여러 줄 주석?
+대입하면 여러줄 문자열로 인식?'''
+
+str = "class 502"
+print(str)
+a = len(str)
+print("length:", a)
+'''class 502
+fighting!!'''
+str1= """\"LGTM!\""""
+print(str1)
