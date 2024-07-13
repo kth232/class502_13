f3d5c86acd1a952ab6803736fcb3770ee56b2e5f
kth232 <kth7537@naver.com>
2024-07-13 17:26:17 +0900

    spring AOP

--- 11. 프레임워크 프로그래밍/day03_1/src/main/java/exam01/CalculatorHandler.java
+++ 11. 프레임워크 프로그래밍/day03_1/src/main/java/exam01/CalculatorHandler.java
@@ -11,6 +11,13 @@ public class CalculatorHandler implements InvocationHandler {
         this.obj = obj; //생성자 매개변수로 원래 객체 
     }
 
+    /**
+     * @param proxy 프록시 자신
+     * @param method 호출한 메소드
+     * @param args 메소드를 호출할 때 전달할 인수
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //proxy는 가짜 객체->인터페이스 기반이기 때문에 인터페이스를 구현하는 프록시 형태 임시 클래스
