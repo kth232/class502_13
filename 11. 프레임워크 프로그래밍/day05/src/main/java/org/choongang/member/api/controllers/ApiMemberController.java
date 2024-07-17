582c830abf0d1562983cd7eb8f35c6d9d625fc2f
kth232 <kth7537@naver.com>
2024-07-17 17:06:31 +0900

    spring JSON 응답과 요청 처리

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -6,6 +6,9 @@ import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
 import org.choongang.member.services.JoinService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,8 +25,12 @@ public class ApiMemberController {
     private final JoinService joinService;
 
     @PostMapping("") //POST /api/member
-    public void join(RequestJoin form) {
-        log.info(form.toString());
+    public ResponseEntity join(@RequestBody RequestJoin form) { //@RequestBody 추가해야 json형식으로 데이터 변환
+        //log.info(form.toString());
+        joinService.process(form); //회원가입 서비스 추가
+
+        //응답 코드가 201, 출력 바디 데이터x
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/info/{email}") //경로 변수
@@ -36,7 +43,7 @@ public class ApiMemberController {
     }
 
     @GetMapping("/list")
-    public List<Member> list() {
+    public ResponseEntity<List<Member>> list() {
         List<Member> members = IntStream.rangeClosed(1, 10)
                 .mapToObj(i->Member.builder() //멤버 객체로 바꿔줌
                         .email("user" + i + "@test.org")
@@ -46,14 +53,18 @@ public class ApiMemberController {
                         .build())
                 .toList();
 
-        return members;
+        HttpHeaders headers  = new HttpHeaders(); //헤더
+        headers.add("t1", "v1");
+        headers.add("t2", "v2");
+
+        return new ResponseEntity<>(members, headers, HttpStatus.OK); //상태코드 201
     }
 
     @GetMapping(path="/test", produces= "text/html;charset=UTF-8") //응답 헤더에 content-type 설정
     public String test() {
         //content-type: text/plain
 
-        return "안녕!";
+        return "안녕!"; //produce 설정하면 한글 깨지지 않음
     }
 
     @GetMapping("/test2")
--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
@@ -7,13 +7,16 @@ import org.choongang.member.controllers.RequestJoin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
@@ -29,39 +32,48 @@ public class ApiMemberControllerTest {
     
     @BeforeEach
     void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        //mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build(); //전체 설정을 가져오면 로딩시간이 오래 걸림
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build(); //특정 컨트롤러만 테스트
+        //mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build(); //전체 설정을 가져오면 로딩 시간이 오래 걸림
     }
     
     @Test
     void test1() throws Exception {
-        //content-type: application/x-www-form-urlencoded <-기본 형식
-
-        mockMvc.perform(post("/api/member")
-                .param("email", "user99@test.org")
-                .param("password", "12345678")
-                .param("confirmPassword", "12345678")
-                .param("userName", "user99"))
-                .andDo(print()); //응답 내용 상세 확인 가능
-    }
-
-    @Test
-    void test2() throws Exception {
         //content-type: application/json
-        //json 형태->이름=값&이름=값...
 
-        ObjectMapper om = new ObjectMapper(); //JSON을 자바 객체로, 자바객체를 JSON으로 변환
+        ObjectMapper om = new ObjectMapper(); //JSON을 자바 객체로, 자바 객체를 JSON으로 변환
         //Date & Time은 지원 안되기 때문에 별도로 설정 필요
         om.registerModule(new JavaTimeModule());
 
         RequestJoin form = new RequestJoin();
-        form.setEmail("user99@test.org");
+        form.setEmail("user100@test.org");
         form.setPassword("12345678");
         form.setConfirmPassword("12345678");
         form.setUserName("user99");
         form.setAgree(true);
 
         String json = om.writeValueAsString(form);
-        System.out.println(json);
+        mockMvc.perform(post("/api/member")
+                        .contentType(MediaType.APPLICATION_JSON) //요청 헤더, mediaType을 사용하면 오타 방지에 좋다
+                        .content(json) //request body
+                ).andDo(print())
+                .andExpect(status().isCreated()); //예상값
+
+        /*
+        //content-type: application/x-www-form-urlencoded <-기본 형식
+        //이름=값&이름=값...
+
+        mockMvc.perform(post("/api/member")
+                .param("email", "user99@test.org")
+                .param("password", "12345678")
+                .param("confirmPassword", "12345678")
+                .param("userName", "user99")
+                ).andDo(print()); //응답 내용 상세 확인 가능
+         */
+    }
+
+    @Test
+    void test2() throws Exception {
+        mockMvc.perform(get("/api/member/list"))
+                .andDo(print());
     }
 }
